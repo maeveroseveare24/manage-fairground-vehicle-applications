f8ab1cbb7a2654882ea1d373a69e026d5e9645b9
Maeve-Roseveare <124513059+Maeve-Roseveare@users.noreply.github.com>
2024-12-09 11:11:30 +0000

    added data

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,5 +1,31 @@
 module.exports = {
 
-  // Insert values here
+  "applications": [
+
+    { 
+      "id": 189813,
+      "fullName": "Taylor Swift",
+      "address": {
+          "line1": "13 Cornelia Street",
+          "town": "New York",
+          "postcode": "TS13"
+      },
+
+      "yearVehicleBuilt": 1989,
+      "registrationNumber": 13,
+      "vehicleDescription": "Pink glittery car",
+      "isVehicleLengthExtended": true,
+      "totalVehicleLengthMetres": 22,
+      "hasLivingAccommodation": true,
+      "hasTransportAnimals": true,
+      "hasPetrolDieselGenerator": true,
+      "hasFairgroundRide": true,
+      "totalWeightTonnes": 34,
+      "isVehicleMaxSpeed": true,
+      "isTravelConvoy": true,
+      "vehicleModified": true,
+
+    }
+  ]
 
 }
